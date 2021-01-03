3f16aa24aa00682ce09848fcc98659c2f2c2e637
Mingqian Ye <mingqian.ye@gmail.com>
2021-01-02 18:26:27 -0800

    work

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -5,6 +5,7 @@ from gym_vim.envs.fs import *
 class TestVim(unittest.TestCase):
     def test_sh(self):
         self.assertEqual(b"a\n", sh("echo a"))
+        self.assertRaises(Exception, sh, "blah")
 
 if __name__ == '__main__':
     unittest.main()
