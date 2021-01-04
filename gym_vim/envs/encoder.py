0ba367dab18721245ca41dae6815cc240fe4e95d
Mingqian Ye <mingqian.ye@gmail.com>
2021-01-04 14:41:28 -0800

    refactor

--- gym_vim/envs/encoder.py
+++ gym_vim/envs/encoder.py
@@ -7,45 +7,42 @@ from typing import Tuple, List, Dict, TypeVar, Generic
 T = TypeVar("T")
 
 class Encoder(Generic[T]):
-    label_encoder: LabelEncoder
-    lookup_dict: Dict[str, np.ndarray] = {}
+    _label_encoder: LabelEncoder
+    _lookup_dict: Dict[str, np.ndarray] = {}
 
     def __init__(self, ll: List[T]): 
-        arr0 = Encoder.convert_to_string_arr(ll)
+        ss = Encoder._convert_to_string_arr(ll)
 
-        arr1 = np.array(arr0)
+        self._label_encoder, label_encoded = Encoder._label_encode(
+            np.array(ss)
+        )
 
-        self.label_encoder = LabelEncoder()
-        integer_encoded = self.label_encoder.fit_transform(arr1)
+        onehot_encoded = Encoder._onehot_encode(label_encoded)
 
-        arr2 = integer_encoded.reshape(len(integer_encoded), 1)
-
-        onehot_arr = OneHotEncoder(sparse=False) \
-            .fit_transform(arr2)
-
-        for i, item in enumerate(arr0):
-            self.lookup_dict[item] = onehot_arr[i]
+        for i, item in enumerate(ss):
+            self._lookup_dict[item] = onehot_encoded[i]
 
     def encode(self, item: T) -> np.ndarray:
-        return self.lookup_dict[str(item)]
+        return self._lookup_dict[str(item)]
 
     def decode(self, arr: np.ndarray) -> np.ndarray:
-        return self.label_encoder.inverse_transform([np.argmax(arr)])
+        return self._label_encoder.inverse_transform([np.argmax(arr)])
 
     def size(self) -> int:
-        return len(self.lookup_dict)
+        return len(self._lookup_dict)
 
-    def convert_to_string_arr(ll: List[T]) -> List[str]:
+    def _label_encode(ss: List[str]) -> Tuple[LabelEncoder, np.ndarray]:
+        label_encoder = LabelEncoder()
+        return label_encoder, label_encoder.fit_transform(ss)
+
+    def _onehot_encode(label_encoded: np.ndarray) -> np.ndarray:
+        return OneHotEncoder(sparse=False).fit_transform(
+            label_encoded.reshape(len(label_encoded), 1)
+        )
+
+
+    def _convert_to_string_arr(ll: List[T]) -> List[str]:
         ss = []
         for l in ll:
             ss.append(str(l))
         return ss
-
-def encode(ss: List[str]):
-    arr = np.array(ss)
-    integer_encoded = LabelEncoder().fit_transform(arr)
-
-    onehot_encoded = OneHotEncoder(sparse=False).fit_transform(
-            integer_encoded.reshape(len(integer_encoded), 1)
-    )
-    return onehot_encoded
