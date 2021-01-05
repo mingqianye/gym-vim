f237c146c55fba4527bb0f07057d41ca1f99c911
Mingqian Ye <mingqian.ye@gmail.com>
2021-01-04 16:09:39 -0800

    edit distance

---
+++ gym_vim/envs/edit_distance.py
@@ -0,0 +1,28 @@
+def edit_distance(s1: str, s2: str) -> int:
+    if s1 == s2:
+        return 0
+
+    n = len(s1)
+    m = len(s2)
+
+    if n == 0 or m == 0:
+        return max(n, m)
+
+    # n+1 rows, m+1 cols
+    dp = [[0 for j in range(m+2)] for i in range(n+2)] 
+
+    for i in range(0, n+1):
+        dp[i][0] = i
+
+    for j in range(0, m+1):
+        dp[0][j] = j
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if (s1[i-1] == s2[j-1]):
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+    
+    return dp[n][m]
+
---
+++ tests/test_edit_distance.py
@@ -0,0 +1,20 @@
+import unittest
+
+import gym_vim
+import numpy as np
+from gym_vim.envs.edit_distance import *
+
+class TestEditDistance(unittest.TestCase):
+    def test_distance(self):
+        self.assertEqual(edit_distance("", ""), 0)
+        self.assertEqual(edit_distance("a", "a"), 0)
+        self.assertEqual(edit_distance("a", ""), 1)
+        self.assertEqual(edit_distance("a", "ab"), 1)
+        self.assertEqual(edit_distance("aB", "ab"), 1)
+        self.assertEqual(edit_distance("ab", "ab"), 0)
+        self.assertEqual(edit_distance("ab", "abc"), 1)
+        self.assertEqual(edit_distance("ab C", "ab cd"), 2)
+        self.assertEqual(edit_distance("ab c", "efg"), 4)
+
+if __name__ == '__main__':
+    unittest.main()
