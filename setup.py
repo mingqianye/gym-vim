fc57f684d26d4454e5686362fe2ee019fd169e47
Mingqian Ye <mingqian.ye@gmail.com>
2021-01-05 02:48:27 -0800

    work

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_vim',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=['gym', 'sklearn', 'numpy', 'pynvim']  # And any other dependencies foo needs
 )
