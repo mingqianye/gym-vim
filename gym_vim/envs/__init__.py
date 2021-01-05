4c513aacba9274e56b6cf054f9b5e5e5734b9cb7
Mingqian Ye <mingqian.ye@gmail.com>
2021-01-04 22:59:07 -0800

    work

--- gym_vim/envs/__init__.py
+++ gym_vim/envs/__init__.py
@@ -1,1 +1,7 @@
+import logging
 from gym_vim.envs.vim_env import VimEnv
+from gym.envs.registration import register
+
+
+logger = logging.getLogger(__name__)
+register(id="Vim-v0", entry_point="gym_vim.envs:VimEnv")
