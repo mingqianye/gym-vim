7bcbc8ba2628d5c35a76680357f09b5cfdcf4432
Mingqian Ye <mingqian.ye@gmail.com>
2021-01-05 09:19:23 -0800

    work

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
-setup(name='gym_vim',
-    version='0.0.6',
+setup(
+    name='gym_vim',
+    version='0.0.7',
     url="https://github.com/mingqianye/gym_vim",
     author="Mingqian Ye",
     license="MIT",
+    packages=["gym_vim", "gym_vim.envs"],
     install_requires=['gym', 'sklearn', 'numpy', 'pynvim']
 )
